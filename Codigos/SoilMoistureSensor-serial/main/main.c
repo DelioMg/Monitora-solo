b2b7e01a359c385b9d183c9f011f8b9f368bd026
Délio <97815194+DelioMg@users.noreply.github.com>
2024-10-10 19:38:53 -0300

    Update main.c

--- Codigos/SoilMoistureSensor-serial/main/main.c
+++ Codigos/SoilMoistureSensor-serial/main/main.c
@@ -1,13 +1,118 @@
+#include <string.h>
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
 #include "esp_log.h"
+#include "esp_system.h"
+#include "esp_http_client.h"
+#include "esp_err.h"
 #include "esp_adc/adc_oneshot.h"
 
+#define URL_BASE "https://supervisor-system.onrender.com/esp32/"
+#define WIFI_SSID "LabCCOMP"
+#define WIFI_PASS "##labccomp##!"
+#define REQUEST_INTERVAL_MS (1000)
+
 static const char *TAG = "SoilMoistureSensor-serial";
+static EventGroupHandle_t s_wifi_event_group;
+#define WIFI_CONNECTED_BIT BIT0
+
+adc_oneshot_unit_handle_t adc_handle;
+
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init(void) {
+    s_wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+    
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+}
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
+    switch (evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_ON_DATA:
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                printf("%.*s", evt->data_len, (char*)evt->data);
+            }
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
+void send_http_get(float value) {
+    char value_str[32];
+    char url[128];
+    snprintf(value_str, sizeof(value_str), "%.2f", value);
+    snprintf(url, sizeof(url), "%s%s", URL_BASE, value_str);
+    ESP_LOGI(TAG, "Requisição para a URL: %s", url);
+
+    esp_http_client_config_t config = {
+        .url = url,
+        .event_handler = _http_event_handler,
+        .timeout_ms = 30000
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP GET Status = %d", esp_http_client_get_status_code(client));
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+
+    esp_http_client_cleanup(client);
+}
 
 void app_main(void) {
-    adc_oneshot_unit_handle_t adc_handle;
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    wifi_init();
+
+    // Configuração do ADC
     adc_oneshot_unit_init_cfg_t init_config = {
         .unit_id = ADC_UNIT_1,
         .ulp_mode = ADC_ULP_MODE_DISABLE,
@@ -16,20 +121,30 @@ void app_main(void) {
         .bitwidth = ADC_BITWIDTH_12,
         .atten = ADC_ATTEN_DB_0,
     };
-
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
-    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_6, &config));
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_7, &config));
 
     while (1) {
         int raw = 0;
-        ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_CHANNEL_6, &raw));
+        ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_CHANNEL_7, &raw));
 
-        float voltage = (float)raw / 4095.0 * 3.3;
+        // Cálculo da umidade
+        float voltage = (float)raw / 4095.0 * 1; // Tensão correspondente
+        float humidity = 0.0; // Inicializa a umidade
 
-        ESP_LOGI(TAG, "Raw: %d, Voltage: %.2f V", raw, voltage);
-        printf("Raw: %d, Voltage: %.2f V\n", raw, voltage);
+        // Se a voltagem for 0.2V, define a umidade como 0%
+        if (voltage <= 0.2) {
+            humidity = 0.0;
+        } else if (voltage >= 1.0) {
+            humidity = 100.0;
+        } else {
+            humidity = (voltage - 0.2) / (1.0 - 0.2) * 100; // Cálculo percentual
+        }
 
-        vTaskDelay(pdMS_TO_TICKS(2000));
+        ESP_LOGI(TAG, "Raw: %d, Voltage: %.2f V, Humidity: %.2f%%", raw, voltage, humidity);
+        send_http_get(humidity);
+
+        vTaskDelay(REQUEST_INTERVAL_MS / portTICK_PERIOD_MS);
     }
 
     ESP_ERROR_CHECK(adc_oneshot_del_unit(adc_handle));
